34b882001d9e9ccbd42798e4c98b45c4a401dfd9
rkataine <rkataine@gmail.com>
2024-02-16 00:44:14 +0200

    dark mode

--- src/main/java/org/baseplayer/Controller.java
+++ src/main/java/org/baseplayer/Controller.java
@@ -1,5 +1,6 @@
 package org.baseplayer;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 
@@ -7,7 +8,16 @@ import javafx.scene.layout.AnchorPane;
 public class Controller {
   @FXML
   private AnchorPane drawCanvas;
-
+  private boolean darkMode = false;
+  public void setDarkMode(ActionEvent event) {
+    if (darkMode) {
+      MainApp.stage.getScene().getStylesheets().remove(MainApp.class.getResource("darkmode.css").toExternalForm());
+      darkMode = false;
+    } else {
+      MainApp.stage.getScene().getStylesheets().add(MainApp.class.getResource("darkmode.css").toExternalForm());
+      darkMode = true;
+    }
+  }    
   public void initialize() {
     
     new DrawSampleData(drawCanvas);
